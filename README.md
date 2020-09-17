f1af6927d01208b843900c484bc9c25c865e3669
Gabriel Faundez <gabriel.faundez@gmail.com>
2020-09-16 22:13:05 -0300

    feat(T1/p2): mst kruskal reborn with solution

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 2020-fardust
 Repositorio de tareas de Diseño y Análisis de Algoritmos 2020 de Gabriel Andrés Faúndez Soto .
+
+## Profile commands
+
+- install dependencies: `pipenv install`
+- create profile: `python -m cProfile -o <file>.prof <file>.py [args...]`
+- view profile: `snakeviz <file>.prof`
--- T1/p2.py
+++ T1/p2.py
@@ -1,5 +1,5 @@
 from queue import PriorityQueue  # https://docs.python.org/3/library/queue.html
-from copy import copy
+from math import inf
 
 # Basado en la implementación de kruskal:
 # https://www.geeksforgeeks.org/kruskals-minimum-spanning-tree-algorithm-greedy-algo-2/
@@ -8,10 +8,10 @@ from copy import copy
 class Connection:
     """Class for keeping track of Connections info."""
 
-    def __init__(self, connections, cost, real_cost):
+    def __init__(self, connections, cost, houses):
         self.connections = connections
         self.cost = cost
-        self.real_cost = real_cost
+        self.houses = houses
 
     def __lt__(self, value):
         return self.cost < value.cost
@@ -20,7 +20,7 @@ class Connection:
         return self.connections == value.connections
 
     def __repr__(self):
-        return f"{self.connections[0]} -> {self.connections[1]}"
+        return f"{self.connections[0]} -{self.cost}-> {self.connections[1]}"
 
 
 def find(parent, node):
@@ -44,12 +44,10 @@ def union(parent, rank, x_tree_set, y_tree_set):
         rank[x_tree_root] += 1
 
 
-def kruskal(edges, total_nodes, banned=set(), offset=0):
+def kruskal(edges, total_nodes, towns, provider_cost, banned=set(), offset=0):
     final_edges = []
+    backup = PriorityQueue()
     final_sum = 0
-    backup = list()
-    banned_vertexes = set()
-    banned_vertexes = banned_vertexes | banned
     nodes = [node for node in range(total_nodes)]
     ranks = [0] * total_nodes
 
@@ -57,31 +55,26 @@ def kruskal(edges, total_nodes, banned=set(), offset=0):
     while edge_count < total_nodes - 1 - offset:
 
         cable = edges.get()
-        backup.append(cable)
+        backup.put(cable)
         x_i, x_j = cable.connections
-        if x_j in banned_vertexes or x_i in banned_vertexes:
+        if x_j in banned or x_i in banned:
             continue
         spot_a = find(nodes, nodes[x_i])
         spot_b = find(nodes, nodes[x_j])
-        if (
-            spot_a != spot_b
-            and not (x_i in banned_vertexes)
-        ):
-            edge_count += 1
-            final_sum += cable.real_cost
+        if spot_a != spot_b:
             final_edges.append(cable)
+            edge_count += 1
+            final_sum += provider_cost if cable.cost == 0 else cable.cost
             union(nodes, ranks, spot_a, spot_b)
-            if x_j != total_nodes - 1:
-                banned_vertexes.add(x_i)
-            
-    for cable in backup:
+
+    while not (backup.empty()):
+        cable = backup.get()
         edges.put(cable)
     return final_edges, edges, final_sum
 
 
 def create_graph(town_quantity, provider_cost, cable_unit_cost, house_qty):
     connections = PriorityQueue()
-    extra_connections = list()
     for provider_index in range(town_quantity):
         for town_index in range(town_quantity):
             estimated_cost = (
@@ -89,48 +82,99 @@ def create_graph(town_quantity, provider_cost, cable_unit_cost, house_qty):
             )
             new_connection = Connection(
                 connections=(town_index, provider_index + town_quantity),
-                cost=estimated_cost / int(house_qty[town_index]),
-                real_cost=estimated_cost
+                cost=estimated_cost * house_qty[town_index],
+                houses=house_qty[town_index],
             )
             connections.put(new_connection)
         extra_connection = Connection(
             connections=(provider_index + town_quantity, 2 * town_quantity),
-            cost=provider_cost / int(house_qty[provider_index]),
-            real_cost=provider_cost
+            cost=0,
+            houses=house_qty[town_index],
         )
         connections.put(extra_connection)
-        extra_connections.append(extra_connection)
-    return connections, extra_connections
+    return connections
 
 
 def calc_optimum_internet_provider(
     town_quantity, provider_cost, cable_unit_cost, house_qty
 ):
-    optimum_internet_provider_cost = [0] * (town_quantity + 1)
+    optimum_internet_provider_cost = [inf] * (town_quantity)
 
-    connections, extra_connections = create_graph(
+    connections = create_graph(
         town_quantity, provider_cost, cable_unit_cost, house_qty
     )
-    banned = set()
-    for i in range(town_quantity - 1):
-        banned.add(extra_connections[i].connections[0])
-        mst, connections, final_sum = kruskal(
-            copy(connections),
-            2 * town_quantity + 1,
-            banned=banned,
-            offset=len(banned)
-        )
-        print(mst, final_sum)
+    all_options = set([town_quantity + town for town in range(town_quantity)])
 
-    return "".join(optimum_internet_provider_cost)
+    solve(
+        all_options,
+        town_quantity,
+        provider_cost,
+        connections,
+        optimum_internet_provider_cost,
+    )
+
+    return " ".join((str(i) for i in optimum_internet_provider_cost))
+
+
+def solve(
+    all_options,
+    town_quantity,
+    provider_cost,
+    connections,
+    optimum_internet_provider_cost,
+    banned=set(),
+    viewed=set(),
+):
+    if banned == all_options:
+        return
+    if frozenset(banned) in viewed:
+        return
+    if banned == set():
+        banned = all_options
+    for option in banned:
+        to_banned = banned - {option}
+        mst, connections, final_sum = kruskal(
+            connections,
+            2 * town_quantity + 1,
+            town_quantity,
+            provider_cost,
+            banned=to_banned,
+            offset=len(to_banned),
+        )
+        viewed_towns = set()
+        final_solution = list()
+        for connection in mst:
+            x_i, x_j = connection.connections
+            if not (x_i in viewed_towns) and x_j != 2 * town_quantity:
+                viewed_towns.add(x_i)
+                final_solution.append(connection)
+            else:
+                final_sum -= (
+                    provider_cost if connection.cost == 0 else connection.cost
+                )
+        test_index = town_quantity - len(to_banned) - 1
+        if (
+            final_sum
+            < optimum_internet_provider_cost[test_index]
+        ):
+            optimum_internet_provider_cost[test_index] = final_sum
+        viewed.add(frozenset(banned))
+        solve(
+            all_options,
+            town_quantity,
+            provider_cost,
+            connections,
+            optimum_internet_provider_cost,
+            set() | to_banned,
+        )
 
 
 if __name__ == "__main__":
     import sys
 
     if len(sys.argv) > 1 and sys.argv[1] == "-t":
-        town_quantity, provider_cost, cable_unit_cost = 6, 9, 1
-        house_qty = 9, 11, 3, 2, 7, 6
+        town_quantity, provider_cost, cable_unit_cost = 5, 7, 1
+        house_qty = 1, 5, 3, 2, 4
     else:
         town_quantity, provider_cost, cable_unit_cost = (
             input().strip("\n").split(" ")
